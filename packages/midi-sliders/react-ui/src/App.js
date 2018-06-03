d83deea4672babae86478a2c49bc1b539bc5d1e6
TimSusa <timsusa@altus-insight.de>
2018-06-03 04:26:43 +0200

    Refactor code and make linter happy

--- packages/midi-sliders/react-ui/src/App.js
+++ packages/midi-sliders/react-ui/src/App.js
@@ -1,29 +1,18 @@
 import React, { Component } from 'react'
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography'
-import { withStyles } from '@material-ui/core/styles';
-import withRoot from './withRoot';
+import { withStyles } from '@material-ui/core/styles'
+import withRoot from './withRoot'
 
 import ChannelStripList from './channel-strip-list/ChannelStripList'
 import './App.css'
 
-const CC_MIDI_START_VAL = 60
-
 class App extends Component {
   state = {
-    sliderEntries: [{
-      label: 'label',
-      val: 50,
-      midiCC: CC_MIDI_START_VAL,
-      outputId: '',
-      isNoteOn: false,
-      midiChannel: 1
-    }],
     midiAccess: {},
     availableDrivers: [{ outputId: '', name: '' }]
   }
 
-  constructor(props) {
+  constructor (props) {
     super(props)
     this.detectChromeBrowser()
 
@@ -35,188 +24,22 @@ class App extends Component {
     }
   }
 
-  componentDidMount() {
-    const tmp = window.localStorage.getItem('slider-entries')
-    if (!tmp) {
-      console.warn('Could not load from local storage. Settings not found.')
-      return
-    }
-    console.log(tmp);
-    const me = JSON.parse(tmp)
-    this.setState({ sliderEntries: me })
-
-    window.addEventListener(
-      "beforeunload",
-      this.saveStateToLocalStorage.bind(this)
-    );
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener(
-      "beforeunload",
-      this.saveStateToLocalStorage.bind(this)
-    );
-
-    // saves if component has a chance to unmount
-    this.saveStateToLocalStorage();
-}
-
-  render() {
+  render () {
     const { classes } = this.props
     return (
       <div className={classes.root}>
         <Typography
           className={classes.heading}
-          variant="display2"
+          variant='display2'
           gutterBottom>
           MIDI Sliders
         </Typography>
-        <Button
-          variant='raised'
-          className={this.props.classes.button}
-          onClick={this.addSlider}>
-          <Typography
-            variant="caption">
-            add slider
-          </Typography>
-        </Button>
         <ChannelStripList
-          entries={this.state.sliderEntries}
           availableDrivers={this.state.availableDrivers}
-          handleInputLabel={this.handleInputLabel}
-          handleSliderChange={this.handleSliderChange}
-          handleNoteTrigger={this.handleNoteTrigger}
-          handleNoteToggle={this.handleNoteToggle}
-          handleCcChange={this.handleCcChange}
-          handleDriverSelectionChange={this.handleDriverSelectionChange}
-          handleMidiChannelChange={this.handleMidiChannelChange}
-          handleRemoveClick={this.handleRemoveClick}
+          midiAccess={this.state.midiAccess}
         />
       </div>
     )
-  }
-
-
-
-  handleNoteToggle = (idx, e) => {
-    let { sliderEntries } = this.state
-    sliderEntries[idx].isNoteOn = !sliderEntries[idx].isNoteOn
-
-    const {
-      output,
-      noteOn, 
-      noteOff
-    } = this.getMidiOutputNoteOnOf(idx)
-
-    if (sliderEntries[idx].isNoteOn) {
-      output.send(noteOn);
-    } else {
-      output.send(noteOff);
-    }
-    this.setState({ sliderEntries })
-  }
-
-  handleNoteTrigger = (idx, e) => {
-    const {
-      output,
-      noteOn, 
-      noteOff
-    } = this.getMidiOutputNoteOnOf(idx)
-
-    output.send(noteOn);
-    setTimeout(
-      function () {
-        output.send(noteOff);
-      },
-      100
-    );
-  }
-
-  getMidiOutputNoteOnOf = (idx) => {
-    const {sliderEntries} = this.state
-    const val = sliderEntries[idx].val
-    const midiCC = sliderEntries[idx].midiCC
-    const outputId = sliderEntries[idx].outputId
-    const noteOn = [0x8f + parseInt(sliderEntries[idx].midiChannel, 10), midiCC, parseInt(val, 10)];
-    const noteOff = [0x7f + parseInt(sliderEntries[idx].midiChannel, 10), midiCC, parseInt(val, 10)];
-    const output = this.state.midiAccess.outputs.get(outputId);
-
-    return {
-      output,
-      noteOn, 
-      noteOff
-    }
-  }
-
-  handleInputLabel = (idx, e) => {
-    let tmp = this.state.sliderEntries
-    tmp[idx].label = e.target.value
-    this.setState({ sliderEntries: tmp })
-  }
-
-  handleDriverSelectionChange = (idx, e) => {
-    let tmp = this.state.sliderEntries
-    tmp[idx].outputId = e.target.value
-    this.setState({ sliderEntries: tmp })
-  }
-
-  handleRemoveClick = (idx) => {
-    let sliderEntries = this.state.sliderEntries
-    if (idx > -1) {
-      sliderEntries.splice(idx, 1)
-    }
-    this.setState({
-      sliderEntries
-    })
-  }
-
-  handleCcChange = (e) => {
-    let sliderEntries = this.state.sliderEntries
-    const tmp = e.target.name.split('-')
-    const idx = tmp[tmp.length - 1]
-    sliderEntries[idx].midiCC = e.target.value
-    this.setState({ sliderEntries })
-  }
-
-  handleMidiChannelChange = (idx, e) => {
-    let {sliderEntries} = this.state
-    sliderEntries[idx].midiChannel = e.target.value
-    this.setState({
-      sliderEntries
-    })
-  }
-
-  addSlider = () => {
-    const sliderEntries = this.state.sliderEntries || []
-    const entry = {
-      label: 'label' + (sliderEntries.length + 1),
-      val: 80,
-      midiCC: parseInt(sliderEntries.length > 0 ? sliderEntries[sliderEntries.length - 1].midiCC : 59, 10) + 1, //count up last entry,
-      outputId: this.state.availableDrivers[0].outputId,
-      midiChannel: 1
-    }
-
-    const newEntries = [...sliderEntries, entry]
-    this.setState({
-      sliderEntries: newEntries
-    })
-  }
-
-  handleSliderChange = (val, idx) => {
-    let sliderEntries = this.state.sliderEntries
-    sliderEntries[idx].val = val
-    const midiCC = sliderEntries[idx].midiCC
-    const outputId = this.state.sliderEntries[idx].outputId
-    var ccMessage = [0xaf + parseInt(sliderEntries[idx].midiChannel, 10), midiCC, parseInt(val, 10)];
-    var output = this.state.midiAccess.outputs.get(outputId);
-    output.send(ccMessage);  //omitting the timestamp means send immediately.
-    this.setState({
-      sliderEntries
-    })
-  }
-
-  saveStateToLocalStorage = () => {
-    window.localStorage.setItem('slider-entries', JSON.stringify(this.state.sliderEntries))
   }
 
   onMIDISuccess = (midiAccess) => {
@@ -231,7 +54,6 @@ class App extends Component {
     if (firstInput) {
       firstInput.onmidimessage = this.handleMidiMessage
     }
-
   }
 
   onMIDIFailure = () => {
@@ -254,8 +76,8 @@ class App extends Component {
       case 128: // noteOff
         // noteOff(note);
         break
-      default: 
-        
+      default:
+
       // we could easily expand this switch statement to cover other types of commands such as controllers or sysex
     }
   }
@@ -281,33 +103,33 @@ class App extends Component {
   }
 
   detectChromeBrowser = () => {
-    // please note, 
+    // please note,
     // that IE11 now returns undefined again for window.chrome
     // and new Opera 30 outputs true for window.chrome
     // and new IE Edge outputs to true now for window.chrome
     // and if not iOS Chrome check
     // so use the below updated condition
-    var isChromium = window.chrome;
-    var winNav = window.navigator;
-    var vendorName = winNav.vendor;
-    var isOpera = typeof window.opr !== "undefined";
-    var isIEedge = winNav.userAgent.indexOf("Edge") > -1;
-    var isIOSChrome = winNav.userAgent.match("CriOS");
+    var isChromium = window.chrome
+    var winNav = window.navigator
+    var vendorName = winNav.vendor
+    var isOpera = typeof window.opr !== 'undefined'
+    var isIEedge = winNav.userAgent.indexOf('Edge') > -1
+    var isIOSChrome = winNav.userAgent.match('CriOS')
 
     if (isIOSChrome) {
-      console.log('is Google Chrome on IOS');
-      alert('sry, is Google Chrome on IOS');
+      console.log('is Google Chrome on IOS')
+      alert('sry, is Google Chrome on IOS')
     } else if (
       isChromium !== null &&
-      typeof isChromium !== "undefined" &&
-      vendorName === "Google Inc." &&
+      typeof isChromium !== 'undefined' &&
+      vendorName === 'Google Inc.' &&
       isOpera === false &&
       isIEedge === false
     ) {
-      console.log('is Google Chrome :-)');
+      console.log('is Google Chrome :-)')
     } else {
-      console.log('not Google Chrome');
-      alert('Sry. This App will only work with Google Chrome Browser.');
+      console.log('not Google Chrome')
+      alert('Sry. This App will only work with Google Chrome Browser.')
     }
   }
 }
@@ -320,7 +142,7 @@ const styles = theme => ({
     marginTop: theme.spacing.unit * 2
   },
   button: {
-    margin: theme.spacing.unit,
+    margin: theme.spacing.unit
   },
   input: {
     width: 100,
@@ -338,7 +160,6 @@ const styles = theme => ({
     fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
     lineHeight: '1.375em'
   }
-});
+})
 
-// export default App
-export default withRoot(withStyles(styles)(App));
+export default withRoot(withStyles(styles)(App))
--- packages/midi-sliders/react-ui/src/channel-strip-list/ChannelStripList.jsx
+++ packages/midi-sliders/react-ui/src/channel-strip-list/ChannelStripList.jsx
@@ -9,93 +9,272 @@ import Tooltip from '@material-ui/core/Tooltip'
 import VolumeSlider from '../VolumeSlider'
 import { withStyles } from '@material-ui/core/styles'
 
-const ChannelStripList = ({ classes, entries, availableDrivers, handleInputLabel, handleSliderChange, handleNoteTrigger, handleNoteToggle, handleCcChange, handleDriverSelectionChange, handleRemoveClick, handleMidiChannelChange }) => {
-  if (!entries) return
-  return (
-    <div className={classes.root}>
-      {
-        entries.map((sliderEntry, idx) => {
-          return (
-            <div key={`slider-${idx}`} className={classes.sliderContainer}>
-              <Input
-                className={classes.input}
-                type='text'
-                onChange={handleInputLabel.bind(this, idx)}
-                value={entries[idx].label} />
-              <VolumeSlider
-                value={entries[idx].val}
-                onChange={val => handleSliderChange(val, idx)} />
-              <Typography className={classes.caption}>{entries[idx].val}</Typography>
-              <Button
-                className={classes.button}
-                variant='raised'
-                onClick={handleNoteTrigger.bind(this, idx)}>
-                <Typography
-                  variant='caption'>
-                  trigger<br />note
-                </Typography>
-              </Button>
-              <Button
-                className={classes.button}
-                variant='raised'
-                onClick={handleNoteToggle.bind(this, idx)}>
-                <Typography
-                  variant='caption'>
-                  toggle<br /> Note {entries[idx].isNoteOn ? 'Off ' : 'On'}
-                </Typography>
+const CC_MIDI_START_VAL = 60
 
-              </Button>
-              <Tooltip
-                placement='top'
-                title='You can set a CC Value or Note Message here.'
-              >
-                <Input
-                  className={classes.input}
-                  id='number'
-                  type='number'
-                  name={`input-cc-name-${idx}`}
-                  value={entries[idx].midiCC}
-                  onChange={handleCcChange} />
-              </Tooltip>
-              <br />
-              <Tooltip
-                placement='top'
-                title={getSelectedDriverName(availableDrivers, entries[idx].outputId)}>
-                <FormControl className={classes.formControl}>
-                  {/* <InputLabel htmlFor="cc">CC</InputLabel> */}
-                  <Select
-                    className={classes.select}
-                    onChange={handleDriverSelectionChange.bind(this, idx)}
-                    value={entries[idx].outputId}>
-                    {renderDriverSelection(availableDrivers)}
-                  </Select>
-                </FormControl>
-              </Tooltip>
-              <Tooltip
-                placement='top'
-                title='You can set the MIDI Channel here.'
-              >
-                <Input
-                  className={classes.input}
-                  id='number'
-                  type='number'
-                  name={`input-channel-name-${idx}`}
-                  value={entries[idx].midiChannel}
-                  onChange={handleMidiChannelChange.bind(this, idx)} />
-              </Tooltip>
-              <br />
-              <Button variant='raised' onClick={handleRemoveClick.bind(this, idx)}>
-                <Typography
-                  variant='caption'>
-                  remove
-                </Typography>
-              </Button>
-            </div>
-          )
-        })
-      }
-    </div>
-  )
+class ChannelStripList extends React.Component {
+  state = {
+    sliderEntries: [{
+      label: 'label',
+      val: 50,
+      midiCC: CC_MIDI_START_VAL,
+      outputId: '',
+      isNoteOn: false,
+      midiChannel: 1
+    }]
+  }
+
+  componentDidMount () {
+    const tmp = window.localStorage.getItem('slider-entries')
+    if (!tmp) {
+      console.warn('Could not load from local storage. Settings not found.')
+      return
+    }
+    const me = JSON.parse(tmp)
+    this.setState({ sliderEntries: me })
+
+    window.addEventListener(
+      'beforeunload',
+      this.saveStateToLocalStorage.bind(this)
+    )
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener(
+      'beforeunload',
+      this.saveStateToLocalStorage.bind(this)
+    )
+
+    // saves if component has a chance to unmount
+    this.saveStateToLocalStorage()
+  }
+
+  render () {
+    return (
+      <div>
+        <Button
+          variant='raised'
+          onClick={this.addSlider}>
+          <Typography
+            variant='caption'>
+            add slider
+          </Typography>
+        </Button>
+        <div className={this.props.classes.channelList}>
+          { this.renderChannelStrips() }
+        </div>
+      </div>
+
+    )
+  }
+
+  renderChannelStrips = () => {
+    const entries = this.state.sliderEntries
+    const {
+      classes,
+      availableDrivers
+    } = this.props
+    return entries && entries.map((sliderEntry, idx) => {
+      return (
+        <div key={`slider-${idx}`} className={classes.sliderContainer}>
+          <Input
+            className={classes.input}
+            type='text'
+            onChange={this.handleInputLabel.bind(this, idx)}
+            value={sliderEntry.label} />
+          <VolumeSlider
+            value={sliderEntry.val}
+            onChange={val => this.handleSliderChange(val, idx)} />
+          <Typography className={classes.caption}>{sliderEntry.val}</Typography>
+          <Button
+            className={classes.button}
+            variant='raised'
+            onClick={this.handleNoteTrigger.bind(this, idx)}>
+            <Typography
+              variant='caption'>
+              trigger<br />note
+            </Typography>
+          </Button>
+          <Button
+            className={classes.button}
+            variant='raised'
+            onClick={this.handleNoteToggle.bind(this, idx)}>
+            <Typography
+              variant='caption'>
+              toggle<br /> Note {sliderEntry.isNoteOn ? 'Off ' : 'On'}
+            </Typography>
+
+          </Button>
+          <Tooltip
+            placement='top'
+            title='You can set a CC Value or Note Message here.'
+          >
+            <Input
+              className={classes.input}
+              id='number'
+              type='number'
+              name={`input-cc-name-${idx}`}
+              value={sliderEntry.midiCC}
+              onChange={this.handleCcChange} />
+          </Tooltip>
+          <br />
+          <Tooltip
+            placement='top'
+            title={getSelectedDriverName(availableDrivers, sliderEntry.outputId)}>
+            <FormControl className={classes.formControl}>
+              {/* <InputLabel htmlFor="cc">CC</InputLabel> */}
+              <Select
+                className={classes.select}
+                onChange={this.handleDriverSelectionChange.bind(this, idx)}
+                value={sliderEntry.outputId}>
+                {renderDriverSelection(availableDrivers)}
+              </Select>
+            </FormControl>
+          </Tooltip>
+          <Tooltip
+            placement='top'
+            title='You can set the MIDI Channel here.'
+          >
+            <Input
+              className={classes.input}
+              id='number'
+              type='number'
+              name={`input-channel-name-${idx}`}
+              value={sliderEntry.midiChannel}
+              onChange={this.handleMidiChannelChange.bind(this, idx)} />
+          </Tooltip>
+          <br />
+          <Button variant='raised' onClick={this.handleRemoveClick.bind(this, idx)}>
+            <Typography
+              variant='caption'>
+              remove
+            </Typography>
+          </Button>
+        </div>
+      )
+    })
+  }
+  handleNoteToggle = (idx, e) => {
+    let { sliderEntries } = this.state
+    sliderEntries[idx].isNoteOn = !sliderEntries[idx].isNoteOn
+
+    const {
+      output,
+      noteOn,
+      noteOff
+    } = this.getMidiOutputNoteOnOf(idx)
+
+    if (sliderEntries[idx].isNoteOn) {
+      output.send(noteOn)
+    } else {
+      output.send(noteOff)
+    }
+    this.setState({ sliderEntries })
+  }
+
+  handleNoteTrigger = (idx, e) => {
+    const {
+      output,
+      noteOn,
+      noteOff
+    } = this.getMidiOutputNoteOnOf(idx)
+
+    output.send(noteOn)
+    setTimeout(
+      function () {
+        output.send(noteOff)
+      },
+      100
+    )
+  }
+
+  getMidiOutputNoteOnOf = (idx) => {
+    const { sliderEntries } = this.state
+    const val = sliderEntries[idx].val
+    const midiCC = sliderEntries[idx].midiCC
+    const outputId = sliderEntries[idx].outputId
+    const midiChannel = parseInt(sliderEntries[idx].midiChannel, 10)
+    const valInt = parseInt(val, 10)
+    const noteOn = [0x8f + midiChannel, midiCC, valInt]
+    const noteOff = [0x7f + midiChannel, midiCC, valInt]
+    const output = this.props.midiAccess.outputs.get(outputId)
+
+    return {
+      output,
+      noteOn,
+      noteOff
+    }
+  }
+
+  handleInputLabel = (idx, e) => {
+    let tmp = this.state.sliderEntries
+    tmp[idx].label = e.target.value
+    this.setState({ sliderEntries: tmp })
+  }
+
+  handleDriverSelectionChange = (idx, e) => {
+    let tmp = this.state.sliderEntries
+    tmp[idx].outputId = e.target.value
+    this.setState({ sliderEntries: tmp })
+  }
+
+  handleRemoveClick = (idx) => {
+    let sliderEntries = this.state.sliderEntries
+    if (idx > -1) {
+      sliderEntries.splice(idx, 1)
+    }
+    this.setState({
+      sliderEntries
+    })
+  }
+
+  handleCcChange = (e) => {
+    let sliderEntries = this.state.sliderEntries
+    const tmp = e.target.name.split('-')
+    const idx = tmp[tmp.length - 1]
+    sliderEntries[idx].midiCC = e.target.value
+    this.setState({ sliderEntries })
+  }
+
+  handleMidiChannelChange = (idx, e) => {
+    let { sliderEntries } = this.state
+    sliderEntries[idx].midiChannel = e.target.value
+    this.setState({
+      sliderEntries
+    })
+  }
+
+  addSlider = () => {
+    const sliderEntries = this.state.sliderEntries || []
+    const entry = {
+      label: 'label' + (sliderEntries.length + 1),
+      val: 80,
+      midiCC: parseInt(sliderEntries.length > 0 ? sliderEntries[sliderEntries.length - 1].midiCC : 59, 10) + 1, // count up last entry,
+      outputId: this.props.availableDrivers[0].outputId,
+      midiChannel: 1
+    }
+
+    const newEntries = [...sliderEntries, entry]
+    this.setState({
+      sliderEntries: newEntries
+    })
+  }
+
+  handleSliderChange = (val, idx) => {
+    let sliderEntries = this.state.sliderEntries
+    sliderEntries[idx].val = val
+    const midiCC = sliderEntries[idx].midiCC
+    const outputId = this.state.sliderEntries[idx].outputId
+    var ccMessage = [0xaf + parseInt(sliderEntries[idx].midiChannel, 10), midiCC, parseInt(val, 10)]
+    var output = this.props.midiAccess.outputs.get(outputId)
+    output.send(ccMessage) // omitting the timestamp means send immediately.
+    this.setState({
+      sliderEntries
+    })
+  }
+
+  saveStateToLocalStorage = () => {
+    window.localStorage.setItem('slider-entries', JSON.stringify(this.state.sliderEntries))
+  }
 }
 
 const getSelectedDriverName = (drivers, outputId) => {
@@ -117,7 +296,7 @@ const renderDriverSelection = (availableDrivers) => {
 }
 
 const styles = theme => ({
-  root: {
+  channelList: {
     display: 'flex',
     marginLeft: theme.spacing.unit * 4
   },
