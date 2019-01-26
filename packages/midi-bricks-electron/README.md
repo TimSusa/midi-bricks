13f229b207b76c672fbcee0c087cad917ee995c1
TimSusa <timsusa@altus-insight.de>
2019-01-26 11:38:45 +0100

    Next shot

---
+++ packages/midi-bricks-electron/README.md
@@ -0,0 +1,19 @@
+
+# midi-sliders electron app
+
+This is just an example of how using electron with a webapp like midi-sliders
+
+Much thanks to https://github.com/HaNdTriX for establishing this!
+
+
+## Install
+
+    npm install
+
+## Development
+
+    npm run dev
+
+## Build (Create Mac and Win Versions to find in dist folder, afterwards)
+
+    npm run build
---
+++ packages/midi-bricks-electron/public/electron.js
@@ -0,0 +1,49 @@
+const { BrowserWindow, app } = require('electron')
+const path = require('path')
+const isDev = require('electron-is-dev')
+const windowStateKeeper = require('electron-window-state')
+let win
+
+// Prevent Zoom, disrupting touches
+app.commandLine.appendSwitch('disable-pinch')
+app.commandLine.appendSwitch('overscroll-history-navigation=0')
+
+app.on('ready', async () => {
+  // Load the previous state with fallback to defaults
+  let mainWindowState = windowStateKeeper({
+    defaultWidth: 1000,
+    defaultHeight: 800
+  })
+
+  // Create the window using the state information
+  win = new BrowserWindow({
+    'x': mainWindowState.x,
+    'y': mainWindowState.y,
+    'width': mainWindowState.width,
+    'height': mainWindowState.height,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // Let us register listeners on the window, so we can update the state
+  // automatically (the listeners will be removed when the window is closed)
+  // and restore the maximized or full screen state
+  mainWindowState.manage(win)
+
+  win.webContents.openDevTools()
+
+  win.webContents.session.setPermissionRequestHandler((webContents, permission, callback) => {
+    console.log('granted permission:', permission)
+    // eslint-disable-next-line standard/no-callback-literal
+    callback(true)
+  })
+
+  const url = isDev
+    ? 'http://localhost:3000/'
+    : `file://${path.join(__dirname, '../build/index.html')}`
+
+  win.loadURL(url)
+})
+
+app.on('window-all-closed', app.quit)
