cc39b9ba4ed9492159f3669b6aff29a07702e573
TimSusa <timsusa@altus-insight.de>
2019-01-26 13:04:41 +0100

    Establish heroku

--- packages/midi-sliders/react-ui/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-
-import * as sliderListReducer from './slider-list'
-import * as viewSettingsReducer from './view-settings'
-
-export default combineReducers({
-  ...sliderListReducer,
-  ...viewSettingsReducer,
-})
