74f914d12ce8f5f44ccfd3d7a68627ab0e981d7e
TimSusa <timsusa@altus-insight.de>
2019-01-26 21:29:22 +0100

    0.12.12

--- packages/midi-bricks-electron/public/electron.js
+++ packages/midi-bricks-electron/public/electron.js
@@ -1,16 +1,14 @@
-const { BrowserWindow, app, autoUpdater } = require('electron')
-const path = require('path')
-const isDev = require('electron-is-dev')
-const windowStateKeeper = require('electron-window-state')
+import { BrowserWindow, app } from 'electron'
+import path from 'path'
+import isDev from 'electron-is-dev'
+import windowStateKeeper from 'electron-window-state'
 
 let win
 
-const server = 'https://hazel-al9wqrth9.now.sh'
-const feed = `${server}/update/${process.platform}/${app.getVersion()}`
+// const server = 'https://hazel-euwqzahun.now.sh'
+// const feed = `${server}/update/${process.platform}/${app.getVersion()}`
 
-console.log('SFda ', feed)
-
-autoUpdater.setFeedURL(feed)
+//autoUpdater.setFeedURL(feed)
 
 // Prevent Zoom, disrupting touches
 app.commandLine.appendSwitch('disable-pinch')
@@ -49,6 +47,7 @@ app.on('ready', async () => {
 
   const url = isDev
     ? 'http://localhost:3000/'
+    // eslint-disable-next-line
     : `file://${path.join(__dirname, '../build/index.html')}`
 
   win.loadURL(url)
