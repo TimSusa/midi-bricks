3db91daf5be945afffceced4883dcc75d011bbdb
TimSusa <timmsusa@googlemail.com>
2019-04-13 16:03:52 +0200

    Improve linting and formatting

--- packages/midi-bricks-electron/public/electron.js
+++ packages/midi-bricks-electron/public/electron.js
@@ -1,54 +1,46 @@
-const { BrowserWindow, app, ipcMain } = require("electron");
-const path = require("path");
-const isDev = require("electron-is-dev");
-const windowStateKeeper = require("electron-window-state");
-require("electron").process;
+const { BrowserWindow, app } = require('electron')
+const path = require('path')
+const isDev = require('electron-is-dev')
+const windowStateKeeper = require('electron-window-state')
+require('electron').process
 
-let win;
+let win
 
 // Prevent Zoom, disrupting touches
-!isDev && app.commandLine.appendSwitch("disable-pinch");
-!isDev && app.commandLine.appendSwitch("overscroll-history-navigation=0");
+!isDev && app.commandLine.appendSwitch('disable-pinch')
+!isDev && app.commandLine.appendSwitch('overscroll-history-navigation=0')
 
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on("ready", createWindow);
+app.on('ready', createWindow)
 
 // Quit when all windows are closed.
-app.on("window-all-closed", function() {
+app.on('window-all-closed', function() {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== "darwin") app.quit();
-});
+  if (process.platform !== 'darwin') app.quit()
+})
 
-app.on("activate", function() {
+app.on('activate', function() {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (win === null) createWindow();
-});
+  if (win === null) createWindow()
+})
 
 function createWindow() {
+  // Extract CLI parameter: Window Coordinates
+  const windowIndex = process.argv.findIndex(item => item === '--window') + 1
+  const [xx, yy, w, h] = process.argv[windowIndex].split(',')
 
-  // Extract CLI parameter
-
-  //
-  // Window Coordinates
-  //
-  const windowIndex = process.argv.findIndex((item) => item === "--window") + 1;
-  const [xx,yy,w,h] = process.argv[windowIndex].split(",");
-
-  //
-  // Enable Dev Console
-  //
-  const devElem = process.argv.find((item) => item === "--dev");
-  console.log('is Dev mode ?: ', !!devElem || isDev)
-  const isDevCli = !!devElem
+  // Extract CLI parameter: Enable Dev Console
+  const isDevelopmentCli =
+    isDev || !!process.argv.find(item => item === '--dev')
 
   // Load the previous state with fallback to defaults
   const mainWindowState = windowStateKeeper({
     defaultWidth: 1000,
     defaultHeight: 800
-  });
+  })
 
   const { x, y, width, height } =
     {
@@ -56,7 +48,7 @@ function createWindow() {
       y: parseInt(yy, 10),
       width: parseInt(w, 10),
       height: parseInt(h, 10)
-    } || mainWindowState;
+    } || mainWindowState
 
   // Create the window using the state information
   win = new BrowserWindow({
@@ -67,37 +59,35 @@ function createWindow() {
     webPreferences: {
       nodeIntegration: true
     },
-    icon: path.join(__dirname, "icons/512x512.png")
-  });
+    icon: path.join(__dirname, 'icons/512x512.png')
+  })
 
   // Let us register listeners on the window, so we can update the state
   // automatically (the listeners will be removed when the window is closed)
   // and restore the maximized or full screen state
-  mainWindowState.manage(win);
+  mainWindowState.manage(win)
 
-  isDev || isDevCli && win.webContents.openDevTools();
+  // Check for develper console
+  isDevelopmentCli && win.webContents.openDevTools()
 
   win.webContents.session.setPermissionRequestHandler(
     (webContents, permission, callback) => {
-      console.log("granted permission:", permission);
       // eslint-disable-next-line standard/no-callback-literal
-      callback(true);
+      callback(true)
     }
-  );
+  )
 
   const url = isDev
-    ? "http://localhost:3000/"
-    : `file://${path.join(__dirname, "../build/index.html")}`;
+    ? 'http://localhost:3000/'
+    : `file://${path.join(__dirname, '../build/index.html')}`
 
-  console.log("load url");
-  win.loadURL(url);
+  win.loadURL(url)
 
   // Emitted when the window is closed.
-  win.on("closed", function() {
-    console.log("close window");
+  win.on('closed', function() {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    win = null;
-  });
+    win = null
+  })
 }
