641a64db4cd16ef5743e589e90ed69f2a8d5608c
TimSusa <timmsusa@googlemail.com>
2019-05-03 15:02:34 +0200

    feature: Establish Lock switch, to avoid overwriting the user set window pos and size

---
+++ packages/midi-bricks/src/components/midi-settings/elements/ValueInput.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import Input from '@material-ui/core/Input'
+import InputLabel from '@material-ui/core/InputLabel'
+import FormControl from '@material-ui/core/FormControl'
+import PropTypes from 'prop-types'
+import InputAdornment from '@material-ui/core/InputAdornment'
+import { makeStyles } from '@material-ui/styles'
+import { Tooltip } from '@material-ui/core'
+
+ValueInput.propTypes = {
+  label: PropTypes.string,
+  limitVal: PropTypes.number,
+  name: PropTypes.string,
+  onChange: PropTypes.func,
+  toolTip: PropTypes.string,
+  value: PropTypes.number,
+  isDisabled: PropTypes.bool
+}
+
+ValueInput.displayName = 'ValueInputComponent'
+
+export function ValueInput({
+  label,
+  isDisabled,
+  value,
+  onChange = () => {},
+  name,
+  limitVal = 0,
+  toolTip,
+  icon
+}) {
+  const classes = makeStyles(styles, { withTheme: true })()
+
+  const form = (
+    <div className={classes.root}>
+      <FormControl className={classes.formControl} disabled={isDisabled}>
+        <InputLabel disabled={isDisabled} className={classes.label}>{`${label}  `}</InputLabel>
+        <Input
+          disabled={isDisabled}
+          className={classes.input}
+          id='someting-s'
+          type='string'
+          name={name}
+          value={value && value}
+          onChange={onChange}
+          startAdornment={
+            <InputAdornment position='start'>{icon && icon}</InputAdornment>
+          }
+        />
+      </FormControl>
+    </div>
+  )
+  const tip = <Tooltip title={toolTip}>{form}</Tooltip>
+
+  return toolTip ? tip : form
+}
+
+function styles(theme) {
+  return {
+    root: {},
+    formControl: {
+      margin: theme.spacing(1)
+    },
+    input: {
+      color: theme.palette.primary.contrastText, // 'rgba(0, 0, 0, 0.54)',
+      fontSize: '1rem',
+      fontWeight: 400,
+      fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+      lineHeight: '1.375em'
+    },
+    label: {
+      color: theme.palette.primary.contrastText
+    }
+  }
+}
