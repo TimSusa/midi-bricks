d6fcac32ecf271fe2d414f374e620e9c7994fc08
TimSusa <timmsusa@googlemail.com>
2019-05-06 05:17:34 +0200

    chore: You might not need to return a promise in your thunk

--- packages/midi-bricks/src/actions/add-element.js
+++ packages/midi-bricks/src/actions/add-element.js
@@ -1,18 +1,18 @@
 import { Actions as sliderListActions } from './slider-list'
-import { Actions as viewSettingsActions} from './view-settings'
+import { Actions as viewSettingsActions } from './view-settings'
 
 const { addPage } = sliderListActions
-const {updateViewSettings} = viewSettingsActions
+const { updateViewSettings } = viewSettingsActions
 
-export  function addElement(mode) {
+export function addElement(mode) {
   return function(dispatch, getState) {
-    return new Promise(async (resolve, reject) => {
-      if (mode === 'PAGE') {
-        dispatch(addPage())
-        const {viewSettings, sliders: {sliderList}} = getState()
-        resolve(dispatch(updateViewSettings({viewSettings, sliderList})))
-      }
-      reject(()=>{})
-    })
+    if (mode === 'PAGE') {
+      dispatch(addPage())
+      const {
+        viewSettings,
+        sliders: { sliderList }
+      } = getState()
+      dispatch(updateViewSettings({ viewSettings, sliderList }))
+    }
   }
 }
