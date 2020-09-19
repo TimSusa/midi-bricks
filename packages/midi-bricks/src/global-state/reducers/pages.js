9f4309d685b165ba4f960289ff932ca8dbd881a7
TimSusa <timmsusa@googlemail.com>
2020-09-19 23:10:11 +0200

    refactor: Out create next state in pages

--- packages/midi-bricks/src/global-state/reducers/pages.js
+++ packages/midi-bricks/src/global-state/reducers/pages.js
@@ -1,40 +1,31 @@
-import { createNextState } from '@reduxjs/toolkit'
 import { pagesInit } from '.'
 
 export const pages = {
-  createPage(state, action) {
+  createPage(draftState, action) {
     const { id } = action.payload
 
-    return createNextState(state, (draftState) => {
-      draftState[id] = {
-        sliderList: [],
-        id,
-        label: `Page ${Object.keys(state).length + 1}`
-      }
-      return draftState
-    })
+    draftState[id] = {
+      sliderList: [],
+      id,
+      label: `Page ${Object.keys(draftState).length + 1}`
+    }
+    return draftState
   },
-  updatePages(state, action) {
-    const { pages } = action.payload
-    return createNextState(state, (draftState) => {
-      draftState = pages
-      return draftState
-    })
+  updatePages(draftState, action) {
+    draftState = action.payload.pages
+    return draftState
   },
-  updateSliderListOfPage(state, action) {
+  updateSliderListOfPage(draftState, action) {
     const { lastFocusedPage, sliderList } = action.payload
-    return createNextState(state, (draftState) => {
-      if (lastFocusedPage && Array.isArray(sliderList)) {
-        draftState[lastFocusedPage].sliderList = sliderList
-      }
-      return draftState
-    })
+
+    if (lastFocusedPage && Array.isArray(sliderList)) {
+      draftState[lastFocusedPage].sliderList = sliderList
+    }
+    return draftState
   },
 
-  deletePages(state) {
-    return createNextState(state, (draftState) => {
-      draftState = pagesInit
-      return draftState
-    })
+  deletePages(draftState) {
+    draftState = pagesInit
+    return draftState
   }
 }
